1b9c10cd6e334add8206570a8f9d208c64e8a78c
myjhye <t01087633799@gmail.com>
2025-04-17 22:59:33 +0900

    feat: enable CORS and update server host for external access from Expo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors"; // ✅ 추가
 import authRouter from "./routes/auth";
 import "./db/index";
 import formidable from "formidable";
@@ -7,6 +8,7 @@ import productRouter from "./routes/product";
 
 const app = express();
 
+app.use(cors()); // ✅ 추가
 app.use(express.static("src/public")); // 정적 파일(HTML, CSS, JavaScript 등)을 제공할 디렉토리를 설정
 app.use(express.json()); // JSON 형식의 요청 본문을 파싱하는 미들웨어를 설정 (앱이 JSON 형식의 데이터를 이해할 수 있게 도움)
 app.use(express.urlencoded({ extended: false })); // URL-encoded 형식의 요청 본문을 파싱하는 미들웨어를 설정 (extended: false는 단순한 형식만 허용) (HTML 폼에서 입력한 데이터(예: 이름, 비밀번호 등)를 서버에서 쉽게 사용할 수 있도록 변환)
@@ -31,5 +33,5 @@ app.post("/upload-file", async (req, res) => {
 });
 
 app.listen(8000, () => {
-    console.log("The app is running on http://localhost:8000")
+    console.log("The app is running on http://0.0.0.0:8000");
 })
