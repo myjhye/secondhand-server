eaf4e6a93498b976b6cf697f2b7ff5574c3add19
myjhye <t01087633799@gmail.com>
2025-05-17 16:45:07 +0900

    feat: integrate socket.io with http server for real-time client connection

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,57 @@
+// 기본 모듈 및 라이브러리 import
 import express from "express";
-import cors from "cors"; // ✅ 추가
-import authRouter from "./routes/auth";
-import "./db/index";
+import cors from "cors";
 import formidable from "formidable";
 import path from "path";
+import http from "http";
+import { Server } from "socket.io";
+
+// 내부 모듈 import
+import "./db/index";
+import authRouter from "./routes/auth";
 import productRouter from "./routes/product";
 
+// Express 앱 및 HTTP 서버 생성
 const app = express();
+const server = http.createServer(app);
 
-app.use(cors()); // ✅ 추가
-app.use(express.static("src/public")); // 정적 파일(HTML, CSS, JavaScript 등)을 제공할 디렉토리를 설정
-app.use(express.json()); // JSON 형식의 요청 본문을 파싱하는 미들웨어를 설정 (앱이 JSON 형식의 데이터를 이해할 수 있게 도움)
-app.use(express.urlencoded({ extended: false })); // URL-encoded 형식의 요청 본문을 파싱하는 미들웨어를 설정 (extended: false는 단순한 형식만 허용) (HTML 폼에서 입력한 데이터(예: 이름, 비밀번호 등)를 서버에서 쉽게 사용할 수 있도록 변환)
+// Socket.IO 서버 설정 (클라이언트는 "/socket-message" 경로로 연결)
+const io = new Server(server, {
+  path: "/socket-message",
+});
+
+// 클라이언트 소켓 연결 이벤트
+io.on("connection", (socket) => {
+    console.log("user is connected");
+} )
+
+// 미들웨어 등록
+app.use(cors()); 
+app.use(express.static("src/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 
-// 1. 
+// 라우터 등록
 app.use("/auth", authRouter);
 app.use("/product", productRouter);
 
 
-// 2. 파일 업로드
+// 파일 업로드 API
 app.post("/upload-file", async (req, res) => {
     const form = formidable({
-        uploadDir: path.join(__dirname, "public"), // 업로드된 파일이 저장될 디렉토리 경로 설정 (__dirname은 현재 스크립트 파일의 디렉토리)
-        filename(name, ext, part, form) { // 저장될 파일의 이름 설정
-            return Date.now() + "_" + part.originalFilename; // 파일명 충돌 방지를 위해 현재 타임스탬프와 원본 파일명을 조합
+        uploadDir: path.join(__dirname, "public"), // 업로드 파일 저장 경로
+        filename(name, ext, part, form) { 
+            return Date.now() + "_" + part.originalFilename; // 파일명 충돌 방지
         },
     });
-    await form.parse(req); // 요청(req)에서 업로드된 파일을 파싱하고 지정된 디렉토리에 저장
+    await form.parse(req); // 파일 파싱 및 저장
 
-    res.send("ok"); // 클라이언트에게 성공 응답 전송
+    res.send("ok"); // 클라이언트 응답
 });
 
-app.listen(8000, () => {
-    console.log("The app is running on http://0.0.0.0:8000");
-})
+
+// 서버 실행
+server.listen(8000, () => {
+  console.log("The app is running on http://0.0.0.0:8000");
+});
